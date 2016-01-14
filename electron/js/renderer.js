ea609ecb143bd28c5d8f8e977e80100d884929b2
geraldarthur <gerald.a.rich@gmail.com>
2016-01-14 18:12:18 -0500

    avoid repeating code and create one canonical source for the renderer

--- electron/js/renderer.js
+++ electron/js/renderer.js
@@ -1,33 +1,10 @@
 var _ = require('lodash')
 var d3 = require('d3')
 
-// TODO: make this requirable from ./processor.js
-//module.exports = HTMLRenderer;
-function HTMLRenderer(config) {
-  console.log("new renderer", config)
-  var results = this.results = {}
-  config.suites.forEach(function(suite) {
-    results[suite.name] = {};
-  })
-}
-
-HTMLRenderer.prototype.error = function(error) {
-  console.log("MAY DAY")
-  console.error(error);
-}
+var HTMLRenderer = require('../src/rendering.js');
 
 HTMLRenderer.prototype.addResult = function(suite, test, result) {
   console.log("add result", suite, test, result)
   this.results[suite][test] = result;
   // TODO: update rendering
 }
-
-HTMLRenderer.prototype.addError = function(suite, test, error) {
-
-}
-
-HTMLRenderer.prototype.done = function() {
-  // finish up
-  console.log("proofed.")
-}
-
