06d2784cbbff43b88f17c0a857aa596cf6b3750b
Ian Johnson <enjalot@gmail.com>
2016-01-14 13:20:12 -0500

    sketch processor in electron. tweak ui a little

---
+++ electron/js/processor.js
@@ -0,0 +1,12 @@
+var d3 = require('d3');
+
+// TODO: handle reload button
+d3.select("#file-loader").on("change", function(evt) {
+
+  // TODO: process file
+  // TODO: call processing functions
+  // TODO: orchestrate rendering in renderer.js
+
+
+
+})
