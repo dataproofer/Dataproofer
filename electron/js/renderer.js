80c88dc3d6fdfab6a76a031ffe0643024e4526c5
Ian Johnson <enjalot@gmail.com>
2016-01-15 15:38:04 -0500

    render fingerprint

--- electron/js/renderer.js
+++ electron/js/renderer.js
@@ -24,6 +24,11 @@ HTMLRenderer.prototype.addResult = function(suite, test, result) {
   var testsEnter = tests.enter().append("div").classed("test", true)
   testsEnter.append("div").classed("passfail", true)
   testsEnter.append("div").classed("message", true)
+  testsEnter.append("div").classed("fingerprint", true).each(function(d) {
+    if(d.result.highlightCells) {
+      d3.select(this).append("canvas")
+    }
+  })
 
   tests.select("div.passfail").html(function(d) { 
     return d.result.passed ? "<span class='icon icon-check'></span>" : "<span class='icon icon-canceled-circle'></span>" 
@@ -33,6 +38,30 @@ HTMLRenderer.prototype.addResult = function(suite, test, result) {
     return d.result.template || d.result.message
   })
 
-  tests.attr({
+  tests.select("div.fingerprint").each(function(d) {
+    if(!d.result.highlightCells) return;
+
+    // TODO: put this in a component/reusable chart thingy
+    var width = 200;
+    var height = 100;
+    var cellWidth = 2;
+    var cellHeight = 1;
+
+    var rows = d.result.highlightCells.slice(0, 500);
+    var cols = Object.keys(rows[0]);
+    cellWidth = width / cols.length;
+    height = cellHeight * rows.length;
+
+    var canvas = d3.select(this).select("canvas").node();
+    var context = canvas.getContext("2d")
+    canvas.width = width;
+    canvas.height = height;
+    
+    rows.forEach(function(row, i) {
+      cols.forEach(function(col, j) {
+        context.fillStyle = row[col] ? "#d88282" : "#ddd";
+        context.fillRect(j*cellWidth, i*cellHeight, cellWidth, cellHeight)
+      })
+    })
   })
 }
