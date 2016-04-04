d900b5ead0ebdf1df331027abe40ba33c2a251df
Ian Johnson <enjalot@gmail.com>
2016-04-04 14:46:28 -0700

    render tests per column

--- electron/js/renderer.js
+++ electron/js/renderer.js
@@ -2,6 +2,7 @@ var _ = require('lodash')
 var d3 = require('d3')
 var jQuery = $ = require('jquery')
 var Renderer = require('dataproofer').Rendering;
+var util = require("dataproofertest-js/util");
 
 function HTMLRenderer(config) {
   //console.log('config', config);
@@ -9,10 +10,7 @@ function HTMLRenderer(config) {
   window.rows = config.rows;
   this.rows = config.rows;
   this.columnHeads = config.columnHeads;
-  var resultList = {}
-  config.suites.forEach(function(suite) {
-    resultList[suite.name] = []
-  })
+  var resultList = []
   this.resultList = resultList;
 
   var data = []
@@ -51,9 +49,10 @@ function HTMLRenderer(config) {
 
   resultsHeight = containerHeight + 'px'
   // we just remove everything rather than get into update pattern
-  d3.select(".step-3-results").selectAll(".suite").remove();
+  d3.select(".step-3-results").selectAll("*").remove();
   d3.select(".step-3-results")
     .style('height', resultsHeight)
+    /*
     .selectAll(".suite")
     .data(config.suites)
     .enter().append("div")
@@ -61,6 +60,7 @@ function HTMLRenderer(config) {
       class: function(d) { return "suite " + d.name + (d.active ? " active" : "" )}
     })
     .append("h2").text(function(d) { return d.fullName })
+    */
   //d3.select(".test-results").selectAll(".test").remove();
 }
 
@@ -69,33 +69,47 @@ HTMLRenderer.prototype.constructor = HTMLRenderer;
 
 HTMLRenderer.prototype.addResult = function(suite, test, result) {
   //console.log("add result", suite, test.name(), result)
-  this.resultList[suite].push({ suite: suite, test: test, result: result || {} })
+  //this.resultList[suite].push({ suite: suite, test: test, result: result || {} })
+  this.resultList.push({ suite: suite, test: test, result: result || {} })
 
-  // setup/update the comments
   var columnHeads = this.columnHeads;
   var rows = this.rows;
   var resultList = this.resultList;
-  var handsOnTable = this.handsOnTable;
-  renderCellComments(rows, columnHeads, resultList, handsOnTable);
 
-  var container = d3.select(".step-3-results ." + suite)
-  var tests = container.selectAll(".test")
-    .data(this.resultList[suite])
+  // setup/update the comments in our Hands On Table
+  renderCellComments(rows, columnHeads, this.resultList, this.handsOnTable);
+
+  var container = d3.select(".step-3-results")
+  // rerender all the columns
+  var columns = container.selectAll(".column")
+    .data(columnHeads)
+
+  var columnsEnter = columns.enter().append("div").classed("column", true)
+  // render the column header
+  columnsEnter.append("div").classed("column-header", true)
+    .text(function(d) { return d})
+
+  var tests = columns.selectAll(".test")
+    .data(function(column) {
+      return resultList.map(function(d) {
+        return { test: d.test, result: d.result, suite: d.suite, column: column}
+      })
+    })
 
   var testsEnter = tests.enter().append("div")
   .attr("class", function(d) {
-     return 'test' + (d.test.active ? " active" : "" )
+     return 'test';// + (d.test.active ? " active" : "" )
   })
   testsEnter.append("div").classed("passfail", true)
-  testsEnter.append("div").classed("message", true)
-  testsEnter.append("div").classed("fingerprint", true).each(function(d) {
-    if(d.result.highlightCells && d.result.highlightCells.length) {
-      d3.select(this).append("canvas")
-    }
-  })
+  testsEnter.append("div").classed("summary", true)
+  testsEnter.append("div").classed("description", true)
+  testsEnter.append("div").classed("conclustion", true)
+  testsEnter.append("div").classed("visualization", true)
 
   tests.on("click", function(d) {
-    console.log(d)
+    console.log(d);
+    var dis = d3.select(this)
+    dis.classed("active", !dis.classed("active"))
   })
 
   tests.select("div.passfail").html(function(d) {
@@ -110,57 +124,32 @@ HTMLRenderer.prototype.addResult = function(suite, test, result) {
     return passFailIconHtml
   })
 
-  tests.select("div.message").html(function(d) {
-
-    var html = '<div class="test-header">' + (d.test.name() || "") + '</div><p>'
-    html += d.result.summary || ""
-    html += "</p>"
-    return html
+  tests.select("div.summary").html(function(d) {
+    var column = d.column;
+    var name = d.test.name();
+    var columnWise = d.result.columnWise || {} // not gauranteed to exist
+    var num = columnWise[column] || 0;
+    var string = name + " " + num + " (" + util.percent(num/rows.length) + ")"
+    return string
+  }).classed("interesting", function(d) {
+    var column = d.column;
+    var columnWise = d.result.columnWise || {} // not gauranteed to exist
+    var num = columnWise[column] || 0;
+    return !!num;
   })
 
-  function drawFingerPrint(d, handsOnTable, that) {
-    var width = 200;
-    var height = 200;
-    var cellWidth = 2;
-    var cellHeight = 1;
+  tests.select("div.description").html(function(d) {
+    return d.test.description()
+  })
 
-    var rows = (d.result.highlightCells > 500) ? d.result.highlightCells.splice(0, 500) : d.result.highlightCells;
-    var cols = Object.keys(rows[0]);
-    cellWidth = width / cols.length;
-    cellHeight = height / rows.length;
-
-    var canvas = d3.select(that).select("canvas").node();
-    var context = canvas.getContext("2d")
-    canvas.width = width;
-    canvas.height = height;
-
-    rows.forEach(function(row, i) {
-      cols.forEach(function(col, j) {
-        context.fillStyle = row[col] ? "#d88282" : "#ddd";
-        context.fillRect(j*cellWidth, i*cellHeight, cellWidth, cellHeight)
-      })
-    })
-
-    var drag = d3.behavior.drag()
-      .on("drag", function(d,i){
-        var mouse = d3.mouse(that);
-        var x = mouse[0];
-        var y = mouse[1];
-        if(y < 0) y = 0;
-        var row = Math.floor(y); // for now our cells are 1 pixel high so this works
-        var col = Math.floor(x / width * cols.length);
-        //console.log("row, col", row, col)
-        handsOnTable.selectCell(row, col, row, col, true);
-      })
-    d3.select(that).select("canvas").call(drag)
-  }
-
+  /*
   var handsOnTable = this.handsOnTable
   tests.select("div.fingerprint").each(function(d) {
     if(!d.result.highlightCells || !d.result.highlightCells.length) return;
     var that = this;
     drawFingerPrint(d, handsOnTable, that);
   })
+  */
 }
 
 HTMLRenderer.prototype.done = function() {
@@ -184,8 +173,9 @@ function renderCellComments(rows, columnHeads, resultList, handsOnTable) {
   });
 
   // loop over resultList
-  Object.keys(resultList).forEach(function(suite) {
-    resultList[suite].forEach(function(d){
+  //Object.keys(resultList).forEach(function(suite) {
+    //resultList[suite].forEach(function(d){
+    resultList.forEach(function(d){
       if(d.result && d.result.highlightCells && d.result.highlightCells.length) {
         _.each(rows, function(row, rowIndex) {
           _.each(columnHeads, function(columnHead) {
@@ -199,7 +189,6 @@ function renderCellComments(rows, columnHeads, resultList, handsOnTable) {
         });
       }
     })
-  })
   _.each(rows, function(row, rowIndex) {
     _.each(columnHeads, function(columnHead, columnIndex) {
       var array = commentCollector[rowIndex][columnHead]
@@ -210,4 +199,43 @@ function renderCellComments(rows, columnHeads, resultList, handsOnTable) {
     });
   });
   handsOnTable.updateSettings({cell: comments})
+}
+
+
+
+function drawFingerPrint(d, handsOnTable, that) {
+  var width = 200;
+  var height = 200;
+  var cellWidth = 2;
+  var cellHeight = 1;
+
+  var rows = (d.result.highlightCells > 500) ? d.result.highlightCells.splice(0, 500) : d.result.highlightCells;
+  var cols = Object.keys(rows[0]);
+  cellWidth = width / cols.length;
+  cellHeight = height / rows.length;
+
+  var canvas = d3.select(that).select("canvas").node();
+  var context = canvas.getContext("2d")
+  canvas.width = width;
+  canvas.height = height;
+
+  rows.forEach(function(row, i) {
+    cols.forEach(function(col, j) {
+      context.fillStyle = row[col] ? "#d88282" : "#ddd";
+      context.fillRect(j*cellWidth, i*cellHeight, cellWidth, cellHeight)
+    })
+  })
+
+  var drag = d3.behavior.drag()
+    .on("drag", function(d,i){
+      var mouse = d3.mouse(that);
+      var x = mouse[0];
+      var y = mouse[1];
+      if(y < 0) y = 0;
+      var row = Math.floor(y); // for now our cells are 1 pixel high so this works
+      var col = Math.floor(x / width * cols.length);
+      //console.log("row, col", row, col)
+      handsOnTable.selectCell(row, col, row, col, true);
+    })
+  d3.select(that).select("canvas").call(drag)
 }
--- src/processing.js
+++ src/processing.js
@@ -22,8 +22,6 @@ exports.run = function(config) {
   var columnHeads = config.columnHeads;
 
   //console.log("processing!", filename, suites)
-
-
   if(!rows && fileString) {
     // Parse the csv with d3
     rows = d3.csv.parse(fileString);
@@ -93,7 +91,7 @@ exports.run = function(config) {
   var result = badColumnHeadsTest.proof(rows, columnHeads)
   renderer.addResult('dataproofer-info-suite', badColumnHeadsTest, result)
 
-  var cleanedcolumnHeads = _.without(columnHeads, result.badColumnHeads.join(', '));
+  var cleanedColumnHeads = _.without(columnHeads, result.badColumnHeads.join(', '));
   var cleanedRows = rows
 
   // TODO: use async series? can run suites in series for better UX?
@@ -105,7 +103,17 @@ exports.run = function(config) {
       if(!test.active) return;
       try {
         // run the test!
-        var result = test.proof(cleanedRows, cleanedcolumnHeads, input)
+        var result = test.proof(cleanedRows, cleanedColumnHeads, input)
+        // aggregate the number of highlighted cells for each column
+        result.columnWise = {};
+        if(result && result.highlightCells) {
+          cleanedColumnHeads.forEach(function(column) {
+            result.columnWise[column] = _.reduce(result.highlightCells, function(count, row) {
+              // if there is a value in this cell, increment count, otherwise leave it alone
+              return !!row[column] ? count + 1 : count
+            }, 0)
+          })
+        }
         // incrementally report as tests run
         renderer.addResult(suite.name, test, result);
       } catch(e) {
