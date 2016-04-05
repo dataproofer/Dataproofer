d55dfb15ece08307705616bbb621cc4759544d32
geraldarthur <gerald.a.rich@gmail.com>
2016-04-04 18:39:28 -0700

    improve initial file detection logic

--- src/processing.js
+++ src/processing.js
@@ -37,6 +37,8 @@ exports.run = function(config) {
       var sheets = xlsx.readFile(filepath).Sheets;
       var firstSheetName = _.keys(sheets)[0];
       rows = xlsx.utils.sheet_to_json(sheets[firstSheetName]);
+    } else {
+      rows = [];
     }
   }
   if(!columnHeads || !columnHeads.length) {
