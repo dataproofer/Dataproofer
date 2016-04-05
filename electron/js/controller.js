69fd00a33337542ce5fffbbe4e28a80d66a432c9
EJ Fox <ejfox@ejfox.com>
2016-04-04 18:19:27 -0700

    Styling updates, horizontal results

--- electron/js/controller.js
+++ electron/js/controller.js
@@ -223,28 +223,31 @@ function renderStep2(processorConfig) {
     });
   var suitesHeds = suitesEnter.append("div")
     .attr("class", "suite-hed");
-  suitesHeds.append("h2")
-    .text(function(d) { return d.fullName; });
-  suitesHeds.append("input")
-    .attr({
-      "class": "toggle",
-      "type": "checkbox",
-      "id": function(d,i){return "suite-" + i;}
-    }).each(function(d) {
-      if(d.active) {
-        d3.select(this).attr("checked", true);
-      } else {
-        d3.select(this).attr("checked", null);
-      }
-    });
-  suitesHeds.append("label")
-    .attr("for", function(d,i){return "suite-" + i;})
-    .on("click", function(d) {
-      d.active = !d.active;
-      d3.select(this.parentNode.parentNode).classed("active", d.active);
-      console.log("suite", d);
-      saveTestConfig();
-    });
+
+    suiteHedAndToggle = suitesHeds.append("h2")
+      .text(function(d) { return d.fullName; });
+
+    suiteHedAndToggle.append("input")
+          .attr({
+            "class": "toggle",
+            "type": "checkbox",
+            "id": function(d,i){return "suite-" + i;}
+          }).each(function(d) {
+            if(d.active) {
+              d3.select(this).attr("checked", true);
+            } else {
+              d3.select(this).attr("checked", null);
+            }
+          });
+
+      suiteHedAndToggle.append("label")
+          .attr("for", function(d,i){return "suite-" + i;})
+          .on("click", function(d) {
+            d.active = !d.active;
+            d3.select(this.parentNode.parentNode).classed("active", d.active);
+            console.log("suite", d);
+            saveTestConfig();
+          });
 
   // render the tests
   var tests = suitesEnter.selectAll(".test")
@@ -252,6 +255,16 @@ function renderStep2(processorConfig) {
 
   var testsEnter = tests.enter().append("div")
     .attr("class", function(d) { return d.active ? "test active" : "test"; });
+
+  testsEnter.append("button").classed("delete-test", true)
+    .text("Delete test")
+    .style("display", function(d) {
+      if(d.filename) return "block";
+      return "none";
+    })
+    .on("click", function(d) {
+      deleteTest(d);
+    });
 
   var onOff = testsEnter.append("div").classed("onoff", true);
   onOff.append("input")
@@ -293,23 +306,18 @@ function renderStep2(processorConfig) {
     .on("click", function(d) {
       renderTestEditor(d);
     });
-  testsEnter.append("button").classed("delete-test", true)
-    .text("Delete test")
-    .style("display", function(d) {
-      if(d.filename) return "block";
-      return "none";
-    })
-    .on("click", function(d) {
-      deleteTest(d);
-    });
+
+  
+  /*
   testsEnter.append("button").classed("duplicate-test", true)
     .text(function(d) {
       if(d.local) return "Duplicate test";
-      return "Duplicate to local suite";
+      return "Make a copy";
     })
     .on("click", function(d) {
       duplicateTest(d);
     });
+    */  
 
   d3.select("#current-file-name").text(processorConfig.filename);
 
@@ -523,7 +531,7 @@ function renderTestEditor(test) {
       hideEditor();
     });
   if (test.local) {
-    saveTest.style("display", "block");
+    saveTest.style("display", "inline-block");
   }
 
   testEditor.style("display", "block");
