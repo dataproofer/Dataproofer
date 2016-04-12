e5d0a313e31dc253af2d4b6833050bf0817f5bc6
geraldarthur <gerald.a.rich@gmail.com>
2016-04-12 13:25:35 -0700

    add topbar

--- electron/js/renderer.js
+++ electron/js/renderer.js
@@ -26,7 +26,6 @@ function HTMLRenderer(config) {
   var handsOnTable = new Handsontable(document.getElementById("grid"),
     {
       data: data,
-      strechH: "all",
       autoWrapRow: true,
       autoWrapCol: true,
       wordWrap: false,
