24a9c8c2c874d3ef12119862ff078523a86ef269
geraldarthur <gerald.a.rich@gmail.com>
2016-10-14 10:12:41 -0700

    update merge script

--- update.sh
+++ update.sh
@@ -3,21 +3,20 @@
 # update all the repos
 # use git pull without specifying origin in case you are working from a branch
 
-git pull
+git pull origin master
 cd ..
 
 cd dataproofertest-js
-git pull
+git pull origin master
 
 cd ../info-suite
-git pull
+git pull origin master
 
 cd ../core-suite
-git pull
+git pull origin master
 
 cd ../stats-suite
-git pull
+git pull origin master
 
 cd ../geo-suite
-git pull
-
+git pull origin master
